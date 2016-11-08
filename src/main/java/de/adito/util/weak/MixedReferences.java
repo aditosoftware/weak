195f7009de6e1e3bef791e7e717e05dd2e81fcd0
j.boesl <j.boesl@adito.de>
2016-11-08 19:23:55 +0100

    Added MixedReferences.

---
+++ src/main/java/de/adito/util/weak/MixedReferences.java
@@ -0,0 +1,138 @@
+package de.adito.util.weak;
+
+import javax.annotation.*;
+import java.lang.ref.*;
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * @author j.boesl, 08.11.16
+ */
+public class MixedReferences<T> implements Iterable<T>
+{
+
+  private final Set set;
+
+  public MixedReferences()
+  {
+    set = Collections.synchronizedSet(new LinkedHashSet<>());
+  }
+
+  @Nonnull
+  @Override
+  public Iterator<T> iterator()
+  {
+    return getObjects().iterator();
+  }
+
+  @Override
+  public void forEach(@Nonnull Consumer<? super T> pAction)
+  {
+    Objects.requireNonNull(pAction);
+    getObjects().forEach(pAction);
+  }
+
+  public boolean isEmpty()
+  {
+    boolean empty;
+    synchronized (set) {
+      empty = set.isEmpty();
+    }
+    return empty;
+  }
+
+  public void addWeak(@Nonnull T pObject)
+  {
+    Objects.requireNonNull(pObject);
+    boolean wasEmpty;
+    synchronized (set) {
+      wasEmpty = set.isEmpty();
+      set.add(WeakReferenceFactory.get().create(pObject, this::remove));
+    }
+    if (wasEmpty)
+      availabilityChanged(true);
+  }
+
+  public void addStrong(@Nonnull T pObject)
+  {
+    Objects.requireNonNull(pObject);
+    if (pObject instanceof WeakReference)
+      throw new IllegalArgumentException("Weak references can't be added as strong ones.");
+
+    boolean wasEmpty;
+    synchronized (set) {
+      wasEmpty = set.isEmpty();
+      set.add(pObject);
+    }
+    if (wasEmpty)
+      availabilityChanged(true);
+  }
+
+  public boolean remove(@Nonnull Object pObject)
+  {
+    boolean wasEmpty;
+    boolean wasRemoved;
+    boolean isEmpty;
+    synchronized (set) {
+      wasEmpty = set.isEmpty();
+      wasRemoved = set.remove(pObject);
+      if (!wasRemoved) {
+        Reference<T> reference = findReference(pObject);
+        if (reference != null)
+          wasRemoved = set.remove(reference);
+      }
+      isEmpty = set.isEmpty();
+    }
+    if (!wasEmpty && isEmpty)
+      availabilityChanged(false);
+
+    return wasRemoved;
+  }
+
+  public void clear()
+  {
+    boolean wasEmpty;
+    synchronized (set) {
+      wasEmpty = set.isEmpty();
+      set.clear();
+    }
+    if (!wasEmpty)
+      availabilityChanged(false);
+  }
+
+  @Nullable
+  protected Reference<T> findReference(@Nonnull Object pObject)
+  {
+    Objects.requireNonNull(pObject);
+    if (pObject instanceof WeakReference) {
+      synchronized (set) {
+        for (Object o : set) {
+          if (o instanceof WeakReference && pObject.equals(((WeakReference) o).get()))
+            return (Reference<T>) o;
+        }
+      }
+    }
+    return null;
+  }
+
+  @Nonnull
+  protected List<T> getObjects()
+  {
+    List<T> objects;
+    synchronized (set) {
+      objects = new ArrayList<>(set.size());
+      for (Object o : set) {
+        if (o instanceof WeakReference)
+          o = ((WeakReference) o).get();
+        if (o != null)
+          objects.add((T) o);
+      }
+    }
+    return objects;
+  }
+
+  protected void availabilityChanged(boolean pAvailable)
+  {
+  }
+
+}
---
+++ src/test/java/de/adito/util/weak/Test_MixedReferences.java
@@ -0,0 +1,118 @@
+package de.adito.util.weak;
+
+import org.junit.*;
+
+import javax.annotation.Nonnull;
+import java.lang.ref.WeakReference;
+import java.util.*;
+
+/**
+ * @author j.boesl, 08.11.16
+ */
+public class Test_MixedReferences
+{
+
+  @Test
+  public void simpleTest() throws InterruptedException
+  {
+    StringBuffer strBuf = new StringBuffer();
+
+    MixedReferences<NamedObject> references = new MixedReferences<NamedObject>()
+    {
+      @Override
+      protected void availabilityChanged(boolean pAvailable)
+      {
+        strBuf.append("av+").append(pAvailable).append(":");
+      }
+
+      @Override
+      public void addWeak(@Nonnull NamedObject pObject)
+      {
+        super.addWeak(pObject);
+        strBuf.append("aw+").append(pObject).append(":");
+      }
+
+      @Override
+      public void addStrong(@Nonnull NamedObject pObject)
+      {
+        super.addStrong(pObject);
+        strBuf.append("as+").append(pObject).append(":");
+      }
+
+      @Override
+      public boolean remove(@Nonnull Object pObject)
+      {
+        boolean wasRemoved = super.remove(pObject);
+        if (wasRemoved) {
+          Object o = pObject instanceof WeakReference ?
+              Optional.ofNullable(((WeakReference) pObject).get()).orElse(pObject.getClass().getSimpleName()) :
+              pObject;
+          strBuf.append("re+").append(o).append(":");
+        }
+        return wasRemoved;
+      }
+    };
+
+
+    List<Object> objects = new ArrayList<>();
+    for (int i = 0; i < 20; i++) {
+      NamedObject namedObject = new NamedObject("" + i);
+      objects.add(namedObject);
+      if (i % 2 == 0)
+        references.addWeak(namedObject);
+      else
+        references.addStrong(namedObject);
+    }
+
+    System.gc();
+
+    _waitForEmpty(references, 10);
+
+    Assert.assertEquals("av+true:aw+NO{0}:as+NO{1}:aw+NO{2}:as+NO{3}:aw+NO{4}:as+NO{5}:aw+NO{6}:as+NO{7}:aw+NO{8}:as+NO{9}:aw+NO{10}:as+NO{11}:aw+NO{12}:as+NO{13}:aw+NO{14}:as+NO{15}:aw+NO{16}:as+NO{17}:aw+NO{18}:as+NO{19}:",
+                        strBuf.toString());
+
+    objects = null;
+
+    System.gc();
+
+    _waitForEmpty(references, 100);
+
+    Assert.assertEquals("av+true:aw+NO{0}:as+NO{1}:aw+NO{2}:as+NO{3}:aw+NO{4}:as+NO{5}:aw+NO{6}:as+NO{7}:aw+NO{8}:as+NO{9}:aw+NO{10}:as+NO{11}:aw+NO{12}:as+NO{13}:aw+NO{14}:as+NO{15}:aw+NO{16}:as+NO{17}:aw+NO{18}:as+NO{19}:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:",
+                        strBuf.toString());
+
+    Assert.assertEquals(Arrays.toString(references.getObjects().toArray()), "[NO{1}, NO{3}, NO{5}, NO{7}, NO{9}, NO{11}, NO{13}, NO{15}, NO{17}, NO{19}]");
+
+    references.clear();
+
+    Assert.assertTrue(references.isEmpty());
+
+    Assert.assertEquals("av+true:aw+NO{0}:as+NO{1}:aw+NO{2}:as+NO{3}:aw+NO{4}:as+NO{5}:aw+NO{6}:as+NO{7}:aw+NO{8}:as+NO{9}:aw+NO{10}:as+NO{11}:aw+NO{12}:as+NO{13}:aw+NO{14}:as+NO{15}:aw+NO{16}:as+NO{17}:aw+NO{18}:as+NO{19}:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:re+_WR:av+false:",
+                        strBuf.toString());
+  }
+
+  private void _waitForEmpty(MixedReferences<NamedObject> pReferences, int pCount) throws InterruptedException
+  {
+    for (int i = 0; i < pCount; i++) {
+      Thread.sleep(10);
+      if (pReferences.isEmpty())
+        break;
+    }
+  }
+
+  private static class NamedObject
+  {
+    private String name;
+
+    public NamedObject(String pName)
+    {
+      name = pName;
+    }
+
+    @Override
+    public String toString()
+    {
+      return "NO{" + name + "}";
+    }
+  }
+
+}
